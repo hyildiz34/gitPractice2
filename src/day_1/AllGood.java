4d2a616e44c35b94a48ac4f99249bb9f08ca40a2
hyildiz34 <64268470+hyildiz34@users.noreply.github.com>
2020-05-02 12:37:32 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,8 +5,11 @@ public class AllGood {
     public static void main(String [] args){
 
         //I hope everything is GOOD.
+        
+        //This line is added from Github
 
-
+        
+        
     }
 
 }
