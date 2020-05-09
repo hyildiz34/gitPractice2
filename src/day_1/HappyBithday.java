01a9619450102ce0e5b7aa06eba97715a80fc415
hyildiz34 <64268470+hyildiz34@users.noreply.github.com>
2020-05-09 16:42:22 -0400

    Update HappyBithday.java

--- src/day_1/HappyBithday.java
+++ src/day_1/HappyBithday.java
@@ -3,7 +3,11 @@ package day_1;
 public class HappyBithday {
 
     public static void main(String[] args) {
+       
         System.out.println("Happy birthday B18");
+        
+       
+        System.out.println("Happy birthday B18 again, this line is edit from GitHub");
 
     }
 }
